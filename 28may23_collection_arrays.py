f062f761ad0094bfa9ca9d26262e39022ec727c1
BandsThoBaby <32070633+BandsThoBaby@users.noreply.github.com>
2023-05-28 17:20:15 +0100

    Add files via upload
    A brief description and definition of the 4 Collection Data Types in Python (Also known as Arrays)

---
+++ 28may23_collection_arrays.py
@@ -0,0 +1,21 @@
+"""
+Python Collections (Arrays) 
+
+There are 4 collection data types in Python
+
+List
+Tuple 
+Set
+Dictionary
+
+List - A collection | Ordered | Changeable | Allows duplicates
+Tuple - A collection | Ordered | Unchangeable | Allows duplicates
+Set - A colelction | Unordered | Unchangeable | Unindexed | No duplicateds
+Dictionary - A collection | Ordered | Changeable | No Duplicates
+
+"""
+
+# Note : 
+#       Choosing the appropriate data type for a particular data set is important. 
+#       It could mean retention of meaning and an increase in effiency and security. 
+# Retention of meaning (The ability to retain and resemble what it was originally supposed to look like. )
